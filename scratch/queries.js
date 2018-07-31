69f9b9028ac4de403daf6c8d1ebe9ddf9bbf3285
Rose Sorfa <rose.otter.2015@gmail.com>
2018-07-31 16:28:54 -0500

    added simdb

---
+++ scratch/queries.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('boring', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1003, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.create({
+  'title': 'dogs are better than cats!',
+  'content': 'stuuuuuuuuuuuuf about dogs!',
+}, (err, item) => {
+  if (err) {
+    console.log(err);
+  } if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.delete(1010, (err, item) => {
+  if (err) {
+    console.log(err);
+  } if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
